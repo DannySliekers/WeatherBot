c9c5db7ca1df02594cd1463482f570277a61b3dc
DannySliekers <59461479+DannySliekers@users.noreply.github.com>
2021-08-26 06:17:38 +0200

    Initial commit

---
+++ src/main/java/WeatherBot.java
@@ -0,0 +1,24 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class WeatherBot {
+    private WeatherAPI weather;
+    private TwitterAPI twitter;
+    public WeatherBot(){
+        this.weather = new WeatherAPI();
+        this.twitter = new TwitterAPI();
+        botLoop();
+    }
+
+    private void botLoop(){
+        ScheduledExecutorService executor = Executors
+                .newSingleThreadScheduledExecutor();
+        executor.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                twitter.postTweet(weather.getWeather());
+            }
+        }, 0, 3, TimeUnit.HOURS);
+    }
+}
